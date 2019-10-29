427d6347c26bc480f64c5a801a5532cb6155f0c0
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-10-28 22:20:17 -0700

    Blob Spawner done

---
+++ The Blob/Assets/Scripts/BlobSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobSpawner : MonoBehaviour
+{
+
+    public Vector3 center;
+    public Vector3 size;
+
+    public GameObject BlobPrefab; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnItem();
+        }
+    }
+
+    public void SpawnItem()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2),
+                          Random.Range(-size.z / 2, size.z / 2));
+
+
+
+        Instantiate(BlobPrefab, pos, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(center, size);
+    }
+}
